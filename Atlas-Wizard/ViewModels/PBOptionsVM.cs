91ae8e3d00afdef95c863c1f44a40fb1c96ed02c
Creeper <98064234+TheyCreeper@users.noreply.github.com>
2025-05-23 19:33:01 -0400

    feat: added views and viewmodels

---
+++ Atlas-Wizard/ViewModels/PBOptionsVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using Wpf.Ui.Controls;
+using Wpf.Ui;
+using Wpf.Ui.Extensions;
+
+namespace Atlas_Wizard.ViewModels
+{
+    public class PBOptionsVM 
+    {
+        private IContentDialogService contentDialogService;
+        private string DialogResultText;
+
+        public PBOptionsVM()
+        {
+
+        }
+
+        [RelayCommand]
+        private async Task OnShowDialog(object content)
+        {
+            ContentDialogResult result = await contentDialogService.ShowSimpleDialogAsync(
+                new SimpleContentDialogCreateOptions()
+                {
+                    Title = "Save your work?",
+                    Content = content,
+                    PrimaryButtonText = "Save",
+                    SecondaryButtonText = "Don't Save",
+                    CloseButtonText = "Cancel",
+                }
+            );
+        }
+    }
+}
