17e699dcd32800d5636d630d1cb5576a72247da0
Creeper <98064234+TheyCreeper@users.noreply.github.com>
2025-05-26 16:45:58 -0400

    feat: started adding defender disabling prompts

---
+++ Atlas-Wizard/Utils/CommandPromptHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas_Wizard.Utils
+{
+    public class CommandPromptHelper
+    { 
+       /// <summary>
+       /// Runs any given command in parameters
+       /// </summary>
+       /// <param name="command">command</param>
+       /// <param name="noWindow">True by default</param>
+        public static void RunCommand(string command, bool noWindow = true, bool waitForExit = true)
+        {
+            Process commandPrompt = new Process();
+            commandPrompt.StartInfo.FileName = "cmd.exe";
+            commandPrompt.StartInfo.Arguments = $"/c {command}";
+            commandPrompt.StartInfo.CreateNoWindow = noWindow;
+            commandPrompt.StartInfo.UseShellExecute = false;
+
+            commandPrompt.Start();
+            if (waitForExit) commandPrompt.WaitForExit();
+        }
+    }
+}
---
+++ Atlas-Wizard/Utils/DisableDefender.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlas_Wizard.Utils;
+using Core;
+using Microsoft.Win32;
+using TrustedUninstaller.CLI;
+using TrustedUninstaller.Shared;
+
+
+namespace Atlas_Wizard.Utils
+{
+    public class DisableDefender
+    {
+        public static async Task<List<bool>> GetDefenderToggles()
+        {
+            var result = new List<bool>();
+
+            await Task.Run(() =>
+            {
+                var defenderKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows Defender");
+                var policiesKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender");
+
+                RegistryKey realtimePolicy = null;
+                RegistryKey realtimeKey = null;
+                try
+                {
+                    try
+                    {
+                        realtimePolicy = policiesKey.OpenSubKey("Real-Time Protection");
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
+                    if (realtimePolicy != null)
+                        realtimeKey = realtimePolicy;
+                    else
+                        realtimeKey = defenderKey.OpenSubKey("Real-Time Protection");
+                }
+                catch
+                {
+                    result.Add(false);
+                }
+
+                if (realtimeKey != null)
+                {
+                    try
+                    {
+                        result.Add((int)realtimeKey.GetValue("DisableRealtimeMonitoring") != 1);
+                    }
+                    catch (Exception exception)
+                    {
+                        try
+                        {
+                            realtimeKey = defenderKey.OpenSubKey("Real-Time Protection");
+                            result.Add((int)realtimeKey.GetValue("DisableRealtimeMonitoring") != 1);
+                        }
+                        catch (Exception e)
+                        {
+                            result.Add(true);
+                        }
+                    }
+                }
+
+                try
+                {
+                    RegistryKey spynetPolicy = null;
+                    RegistryKey spynetKey = null;
+
+                    try
+                    {
+                        spynetPolicy = policiesKey.OpenSubKey("SpyNet");
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
+                    if (spynetPolicy != null)
+                        spynetKey = spynetPolicy;
+                    else
+                        spynetKey = defenderKey.OpenSubKey("SpyNet");
+
+                    int reporting = 0;
+                    int consent = 0;
+                    try
+                    {
+                        reporting = (int)spynetKey.GetValue("SpyNetReporting");
+                    }
+                    catch (Exception e)
+                    {
+                        if (spynetPolicy != null)
+                        {
+                            reporting = (int)defenderKey.OpenSubKey("SpyNet").GetValue("SpyNetReporting");
+                        }
+                    }
+
+                    try
+                    {
+                        consent = (int)spynetKey.GetValue("SubmitSamplesConsent");
+                    }
+                    catch (Exception e)
+                    {
+                        if (spynetPolicy != null)
+                        {
+                            consent = (int)defenderKey.OpenSubKey("SpyNet").GetValue("SubmitSamplesConsent");
+                        }
+                    }
+
+                    result.Add(reporting != 0);
+                    result.Add(consent != 0 && consent != 2 && consent != 4);
+                }
+                catch
+                {
+                    result.Add(false);
+                    result.Add(false);
+                }
+
+                try
+                {
+                    int tamper = (int)defenderKey.OpenSubKey("Features").GetValue("TamperProtection");
+                    result.Add(tamper != 4 && tamper != 0);
+                }
+                catch
+                {
+                    result.Add(false);
+                }
+            });
+            return result;
+        }
+
+        public async void CheckForDefender()
+        {
+            if (((AmeliorationUtil.Playbook.Requirements.Contains(Requirements.Requirement.DefenderDisabled) || AmeliorationUtil.Playbook.Requirements.Contains(Requirements.Requirement.DefenderToggled))))
+            {
+                bool first = true;
+
+                while ((await GetDefenderToggles()).Any(x => x))
+                {
+                    
+                }
+            }
+        }
+        public void OpenDefender()
+        {
+            CommandPromptHelper.RunCommand("windowsdefender://");
+        }
+    }
+}
